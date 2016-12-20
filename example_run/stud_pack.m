9e480bf86703be9487ffb1a62a5b923bd078219a
Rudrakh Panigrahi <rudrakh97@gmail.com>
2016-12-20 14:42:43 +0530

    Update stud_pack.m

--- example_run/stud_pack.m
+++ example_run/stud_pack.m
@@ -1,8 +1,9 @@
-filename = 'C:\Users\Rudrakh97\Downloads\ENB2012_data.xlsx';%whatever is the filepath
+%This is a run on example data (not package data)
+filename = 'C:\Users\Rudrakh97\Downloads\ENB2012_data.xlsx';
 var_matrix = xlsread(filename);
 input_var = var_matrix([1:768],[1:8]);%input variable matrix extracted from excel matrix
 output_var = var_matrix([1:768],[9,10]);%output variable matrix extracted from excel matrix
-%The first prediction is using normalisation method
+%The first prediction is using normalization method
 theta_matrix = pinv(transpose(input_var)*input_var)*transpose(input_var)*output_var;%theta = (XTX)^(-1)*XT*Y
 %Using inbuilt function for greater accuracy
 options = optimset('GradObj', 'on' ,'MaxIter', 150);
